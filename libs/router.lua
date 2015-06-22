58d1475af11cd438f3ff7d36ea1c6438fbafeb3b
Cyril Hou <houshoushuai@gmail.com>
2015-06-22 17:37:32 +0800

    [master] fixed router for / query.

--- libs/router.lua
+++ libs/router.lua
@@ -40,7 +40,7 @@ local function match_one_path(node, method, path, f)
 end
 
 local function resolve(path, node, params)
-  local _, _, current_token, newPath = path:find("([^/]+)(.*)(??)")
+  local _, _, current_token, newPath = path:find("/([^/?]+)([^?]*)(?*)")
   if not current_token then return node["LEAF"], params end
 
   for child_token, child_node in pairs(node) do
