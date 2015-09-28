9841f4a28c070df4c0da89a891b95f81f16db97a
Cyril Hou <houshoushuai@gmail.com>
2015-09-28 16:27:57 +0800

    [master] prepair publish to lit

---
+++ package.lua
@@ -0,0 +1,20 @@
+--
+-- Created by Cyril Hou
+-- Date: 15/9/28
+-- Time: 2015-09-28 16:24:56
+--
+return {
+    name = "cyrilis/mooncake",
+    version = "0.0.1",
+    homepage = "https://github.com/cyrilis/luvit-mooncake",
+    description = "Web framework for Luvit lang.",
+    tags = {"luvit", "web framework", "web", "application", "express", "mooncake", "framework"},
+    license = "MIT",
+    author = { name = "Cyril Hou" },
+    files = {
+        "**.lua",
+        "**.md",
+        "**.html"
+    }
+}
+
